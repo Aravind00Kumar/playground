5c31386fe0308b68a89dd2bb6959784978063c95
Aravind00Kumar <AravindKumarmail@gmail.com>
2016-07-10 19:38:35 +0530

    update

--- src/JS/class/class.js
+++ src/JS/class/class.js
@@ -7,16 +7,29 @@
 
     var emp = new Employee(1, "foo");
     console.log("emp is : " + emp.id + ", " + emp.name);
-    console.log('is emp instanceof Employee ? '+ (emp instanceof Employee));
+    console.log('is emp instanceof Employee ? ' + (emp instanceof Employee));
 
     var object = {};
     object.__proto__ = Employee.prototype;
     object.constructor(2, "bar");
-    console.log('is object instanceof Employee ? '+ (object instanceof Employee));
+    console.log('is object instanceof Employee ? ' + (object instanceof Employee));
 
 
     var object2 = Object.create(Employee.prototype);
-    object2.constructor(1,"k");
-    console.log('is object2 instanceof Employee ? '+ (object2 instanceof Employee));
+    object2.constructor(1, "k");
+    console.log('is object2 instanceof Employee ? ' + (object2 instanceof Employee));
+
+
+    var context = "ALIASED LOG:"
+    var logalias;
+
+    if (console.log.bind === 'undefined') { // IE < 10
+        logalias = Function.prototype.bind.call(console.log, console, context);
+    }
+    else {
+        logalias = console.log.bind(console, context);
+    }
+
+    logalias('Hello, world!');
 
 })();
