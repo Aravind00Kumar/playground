3ed9879cebfde1c8349444fa01cd2860c142a5cb
Aravind00Kumar <AravindKumarmail@gmail.com>
2016-07-10 12:55:50 +0530

    added jquery, code mirrie and a class examples

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 (function () {
     'use strict'
     angular.module('app', ['ui.router'])
+        .run(['$rootScope', '$timeout', function ($rootScope, $timeout) {
+
+            $rootScope.$on('$stateChangeSuccess',
+                function (event, toState, toParams, fromState, fromParams) {
+                    $timeout(function () {
+                        var elements = document.querySelectorAll('textarea[name="code"]');
+                        elements.forEach(function (element) {
+                            CodeMirror.fromTextArea(element, {
+                                lineNumbers: true,
+                                matchBrackets: true,
+                                readOnly: true,
+                                viewportMargin: Infinity
+                            });
+                        }, this);
+                    })
+                });
+        }])
+
         // route configuration
         .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             $urlRouterProvider.otherwise("/home");
@@ -19,7 +37,17 @@
                 .state('css.border', {
                     url: "/border",
                     templateUrl: "css/border/css.border.html",
-                });
+                })
+
+                // JAVASCRIPR                
+                .state('js', {
+                    url: "/js",
+                    template: '<ui-view></ui-view>'
+                })
+                .state('js.class', {
+                    url: "/class",
+                    templateUrl: "js/class/js.class.html",
+                })
         }])
 
         .controller('HomeController', [function () {
@@ -32,7 +60,12 @@
             this.title = 'About';
         }]);
 
+
+
     angular.element(document).ready(function () {
         angular.bootstrap(document, ['app']);
-    })
+    });
+
 })();
+
+
